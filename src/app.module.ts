46427e482faf4abeca29b98da08e9408e8168a9b
zhangjinjie <1392372716@qq.com>
2023-09-18 23:30:49 +0800

    feat: init user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SharedModule } from './shared/share.module';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
   imports: [
-    // 业务模块
     // 共享模块
     SharedModule,
+    // 用户模块
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
