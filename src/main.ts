8d943836fd41096c1b257470bb67fcc8f29652d0
zhangjinjie <1392372716@qq.com>
2023-09-24 14:18:24 +0800

    feat: 添加全局的响应格式化拦截器、HTTP异常过滤器

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,14 @@ import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 
 import { AppModule } from './app.module';
+import { FormatResponseInterceptor } from './shared/interceptors/format-response.interceptor';
+import { HttpExceptionFilter } from './shared/filter/http-exception/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalInterceptors(new FormatResponseInterceptor());
+  app.useGlobalFilters(new HttpExceptionFilter());
 
   const configService = app.get(ConfigService);
   const port = configService.get<number>('port') || 3000;
