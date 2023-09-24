04cc3c56925b062f00e98c34aa1372ec938b09b8
zhangjinjie <1392372716@qq.com>
2023-09-24 15:05:22 +0800

    feat: add invoke-record interceptor

---
+++ src/shared/interceptors/invoke-record.interceptor.ts
@@ -0,0 +1,44 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  Logger,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+import { Observable, tap } from 'rxjs';
+
+@Injectable()
+export class InvokeRecordInterceptor implements NestInterceptor {
+  private readonly logger = new Logger(InvokeRecordInterceptor.name);
+
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const request = context.switchToHttp().getRequest<Request>();
+    const response = context.switchToHttp().getResponse<Response>();
+
+    const userAgent = request.headers['user-agent'];
+
+    const { ip, method, path } = request;
+
+    // 请求触发
+    this.logger.debug(
+      `${method} ${path} ${ip} ${userAgent}: ${context.getClass().name} ${
+        context.getHandler().name
+      } invoked...`,
+    );
+
+    const now = Date.now();
+
+    return next.handle().pipe(
+      tap((res) => {
+        // 返回响应
+        this.logger.debug(
+          `${method} ${path} ${ip} ${userAgent}: ${response.statusCode}: ${
+            Date.now() - now
+          }ms`,
+        );
+        this.logger.debug(`Response: ${JSON.stringify(res)}`);
+      }),
+    );
+  }
+}
