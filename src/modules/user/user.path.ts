2bb95eebbd67c6aa5dabe12bc7b5064dcee1422f
Meils.zjj <1392372716@qq.com>
2023-10-07 19:04:28 +0800

    Create user.path.ts

---
+++ src/modules/user/user.path.ts
@@ -0,0 +1,10 @@
+// 用户模块 path 集
+export const UserModulePaths = {
+  base: 'user',
+  contents: {
+    /** @path POST /user/login 用户登录 */
+    login: 'login',
+    /** @path POST /user/info 用户信息查询 */
+    info: 'info',
+  },
+};
